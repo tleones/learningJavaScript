6f197a4fa3ec88fe44cf1b0688cf80e26ee87b0a
Tomás Leones <tomasleones@gmail.com>
2024-01-02 14:39:29 -0300

    Adição de lógica ao jogo: geração automática de número secreto e uso do operador ternário.

--- Projetos/Lógica de programação mergulhe em programação com JavaScript/app.js
+++ Projetos/Lógica de programação mergulhe em programação com JavaScript/app.js
@@ -1,23 +1,23 @@
 alert('Boas-vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = Math.floor(Math.random() * 111) + 1;
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// enquanto
 while(chute != numeroSecreto) {
 
-    chute = prompt('Escolha um número entre 1 e 10');
-    // se chute for igual ao número secreto
+    chute = prompt('Escolha um número entre 1 e 100!');
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
+            alert(`O número secreto é menor que ${chute}.`);
         } else {
-            alert(`O número secreto é maior que ${chute}`);
+            alert(`O número secreto é maior que ${chute}.`);
         }
-        // tentativas = tentativas + 1;
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);

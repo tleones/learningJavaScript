34754221701ec7d395533bf498fe9f8f2a002b21
Tomás Leones <tomasleones@gmail.com>
2024-01-03 09:18:15 -0300

    Inserção do arquivo de exercícios sem resposta do capítulo Funções

---
+++ Exercícios/2. Lógica de programação explore funções e listas/2. Funções/exerciciosNaoRespondidos.js
@@ -0,0 +1,90 @@
+/*-------------------------
+
+45. Marque apenas as opções verdadeiras sobre funções.
+
+A) As funções em JavaScript podem ter um nome.
+
+B) Palavra-chave function: É usada para declarar uma função em JavaScript.
+
+C) Corpo da função []: O corpo da função é delimitado por colchetes e contém o código que será executado quando a função for chamada.
+
+D) Parênteses (): Após o nome da função (se houver), os parênteses são usados para definir os parâmetros da função.
+
+E) Palavra-chave return é obrigatória: A palavra-chave return é usada para especificar o valor que a função deve retornar após ser executada.
+
+-------------------------
+
+46. Você é uma pessoa desenvolvedora de software que trabalha com JavaScript e precisa criar uma função que verifique se um número é positivo, negativo ou zero. 
+Sua tarefa é implementar uma função que recebe como parâmetro um número inteiro e exibe uma mensagem no console conforme as seguintes regras:
+
+Se o número for maior que zero, a mensagem deve ser: "O número é positivo."
+Se o número for menor que zero, a mensagem deve ser: "O número é negativo."
+Se o número for igual a zero, a mensagem deve ser: "O número é zero."
+Você pode utilizar a estrutura de controle if-else para verificar as condições e exibir a mensagem correspondente no console. 
+Sabendo disso, analise as alternativas abaixo e marque aquela que possui o código que atende todas as regras citadas acima.
+
+A)
+function verificarNumero(numero) {
+  if (numero < 0) {
+    console.log("O número é positivo.");
+  } else if (numero > 0) {
+    console.log("O número é negativo.");
+  } else {
+    console.log("O número é zero.");
+  }
+}
+
+B)
+function verificarNumero(numero) {
+  if (numero > 0) {
+    console.log("O número é positivo.");
+  } else {
+    console.log("O número é zero.");
+  }
+}
+
+C)
+if (numero > 0) {
+    console.log("O número é positivo.");
+  } else if (numero < 0) {
+    console.log("O número é negativo.");
+  } else {
+    console.log("O número é zero.");
+  }
+
+D)
+  function verificarNumero(numero) {
+  if (numero > 0) {
+    console.log("O número é positivo.");
+  } else if (numero < 0) {
+    console.log("O número é negativo.");
+  } else {
+    console.log("O número é zero.");
+  }
+}
+
+-------------------------
+
+47.Criar uma função que exibe "Olá, mundo!" no console.
+
+-------------------------
+
+48. Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+-------------------------
+
+49. Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+-------------------------
+
+50. Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+-------------------------
+
+51. Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+-------------------------
+
+52. Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.
+
+-------------------------

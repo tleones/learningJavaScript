1fb3ca97eab8b1639f014ca416561c7143ad3376
Tomás Leones <tomasleones@gmail.com>
2024-01-03 08:40:45 -0300

    Incluindo função com retorno para gerar número

--- Projetos/2. Lógica de programação explore funções e listas/app.js
+++ Projetos/2. Lógica de programação explore funções e listas/app.js
@@ -1,8 +1,4 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10.';
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -14,4 +10,8 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     console.log('O botão foi clicado.');
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }

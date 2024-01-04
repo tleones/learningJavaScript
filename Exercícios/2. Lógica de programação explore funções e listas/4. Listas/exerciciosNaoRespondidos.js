f317e2a9e92678a12a495fdb07ffd79d3e575fdf
Tomás Leones <tomasleones@gmail.com>
2024-01-03 23:50:48 -0300

    Inclusão dos exercícios do capítulo 4 sem respostas

---
+++ Exercícios/2. Lógica de programação explore funções e listas/4. Listas/exerciciosNaoRespondidos.js
@@ -0,0 +1,54 @@
+/*-------------------------
+60. Você foi encarregado (a) de criar um pequeno programa para gerenciar uma lista de compras. 
+O programa deve permitir que o usuário adicione itens à lista e, em seguida, exiba o conteúdo da lista na tela. 
+Além disso, a pessoa usuária deve ser capaz de ver um item específico da lista, digitando o índice do item desejado.
+
+let listaCompras = [];
+
+listaCompras.push("Maçã");
+listaCompras.push("Arroz");
+listaCompras.push("Leite");
+listaCompras.push("Pão");
+listaCompras.push("Carne");
+
+Sabendo disso, analise as alternativas abaixo e marque apenas as sentenças verdadeiras:
+
+Selecione 2 alternativas
+
+A)
+listaCompras.lengthexibe a quantidade de elementos de uma lista.
+
+B)
+O código listaCompras[3] vai exibir o item Pão.
+
+C)
+O código listaCompras[1] vai exibir o item Maçã.
+
+D)
+listaCompras.lenghtexibe a quantidade de elementos de uma lista.
+
+-------------------------
+
+61. Crie uma lista vazia, com o nome listaGenerica.
+
+-------------------------
+
+62. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+-------------------------
+
+63. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+-------------------------
+
+64. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+-------------------------
+
+65. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+-------------------------
+
+66. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+-------------------------*/
